1c1f527b0c5c26c6beb53f988e01f2bee4a132ef
apollo725 <superapollo0725@gmail.com>
2019-05-12 15:07:06 +0200

    last update for client folder

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const cors = require('cors');
+const schema = require('./schema');
+
+const app = express();
+
+// Allow cross-origin
+app.use(cors());
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
